afdddc1cc5ab98c462dd1c68326953fc7419c704
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-06-21 20:00:04 +0530

    Basic Info Site working

--- NodeJS Course/Basic Informational Site/index.js
+++ NodeJS Course/Basic Informational Site/index.js
@@ -1,18 +1,46 @@
-const express = require("express");
-// start and instance of express server
-const app = express();
-const PORT = 3000;
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const HOSTNAME = 'localhost';
 
-const path = require('path');
+const PORT = 3000 || process.env.PORT
 
-// Serve static/public html files
-app.use(express.static(path.join(__dirname, "public")));
+http.createServer(function (req, res) {
+    const reqUrl = url.parse(req.url);
+    const reqPath = reqUrl.pathname;
+    // TODO: replace with route map
+    switch (reqPath) {
+        case "/":
+            returnPageStatus("public/index.html", res)
+            break;
+        case "/about":
+            returnPageStatus("public/about.html", res)
+            break;
+        case "/contact-me":
+            returnPageStatus("public/contact-me.html", res)
+            break;
+        default:
+            returnPageStatus("", res)
+            break;
+    }
 
-// Parse through incoming data through your HTTP requests
-app.use(express.json());
+}).listen(PORT, HOSTNAME, ()=> {
+    console.log(`Server running at ${HOSTNAME}:${PORT}`)
+})
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+function returnPageStatus(fName, res) {
+    // check existance before calling
+    fs.stat(`${fName}`, (err, stats) => {
+        res.setHeader('Content-Type', 'text/html');
+        // if file is found, set header and deliver html file as stream
+        if (stats) {
+            res.statusCode = 200;
+            fs.createReadStream(fName).pipe(res);
+        }
+        else {
+            res.statusCode = 404
+            //TODO: check for this file existance
+            fs.createReadStream(`./404.html`).pipe(res);
+        }
+    });
+}
